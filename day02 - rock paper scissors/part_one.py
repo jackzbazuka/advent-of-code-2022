e34284638adf635f75f7777305c96943f485b0bd
jackzbazuka <milindmoto99@gmail.com>
2022-12-04 23:17:21 +0530

    feat: :sparkles: day02 - rock, paper, scissors

---
+++ day02 - rock paper scissors/part_one.py
@@ -0,0 +1,41 @@
+opponent_score_map = {
+    "A": 1,
+    "B": 2,
+    "C": 3,
+}
+
+player_score_map = {
+    "X": 1,
+    "Y": 2,
+    "Z": 3,
+}
+
+
+def main() -> int:
+    """
+    Returns:int: Total strategy score
+    """
+    score = 0
+
+    with open("strategies.in", "r") as f:
+
+        for strategy in f:
+            opponent, player = strategy.strip().split(" ")
+            score += player_score_map[player]
+
+            if opponent_score_map[opponent] == player_score_map[player]:
+                score += 3
+            else:
+                if abs(opponent_score_map[opponent] - player_score_map[player]) == 2:
+                    if opponent_score_map[opponent] > player_score_map[player]:
+                        score += 6
+                elif (opponent_score_map[opponent] + player_score_map[player]) >= 3:
+                    if opponent_score_map[opponent] < player_score_map[player]:
+                        score += 6
+
+    return score
+
+
+if __name__ == "__main__":
+    strategy_score = main()
+    print(strategy_score)
---
+++ day02 - rock paper scissors/part_two.py
@@ -0,0 +1,48 @@
+opponent_score_map = {
+    "A": 1,
+    "B": 2,
+    "C": 3,
+}
+
+player_score_map = {
+    "X": 1,
+    "Y": 2,
+    "Z": 3,
+}
+
+
+def main() -> int:
+    """
+    Returns:int: Total strategy score
+    """
+    score = 0
+
+    with open("strategies.in", "r") as f:
+
+        for strategy in f:
+            opponent, outcome = strategy.strip().split(" ")
+
+            if outcome == "X":
+                if opponent == "A":
+                    score += 3
+                elif opponent == "B":
+                    score += 1
+                else:
+                    score += 2
+            elif outcome == "Y":
+                score += opponent_score_map[opponent] + 3
+            elif outcome == "Z":
+                score += 6
+                if opponent == "A":
+                    score += 2
+                elif opponent == "B":
+                    score += 3
+                else:
+                    score += 1
+
+    return score
+
+
+if __name__ == "__main__":
+    strategy_score = main()
+    print(strategy_score)
